93a176004a0d4e9975b9b72bfbdcb5a3f09f4f68
Amit Znati <amitznati@gmail.com>
2017-03-28 22:49:09 +0200

    remove tests

--- Tests/POS.Repository.Tests/RepositoryTests.cs
+++ Tests/POS.Repository.Tests/RepositoryTests.cs
@@ -16,37 +16,6 @@ namespace POS.Repository.Tests
             Assert.IsNotNull(emps);
         }
 
-        [TestMethod]
-        public void addNewEmployee()
-        {
-            using(UnitOfWork inst = UnitOfWork.get()){
-            Employee emp = new Employee();
-            emp.FirstName = "שני";
-            emp.LastName = "זנטי2";
-            emp.Password = "1010";
-            inst.Peoples.Add(emp);
-            int empnum = inst.Complete();
-            Assert.IsNotNull( inst.Employees.Get(empnum));
-            //UnitOfWork.get().
-            }
-
-        }
-        [TestMethod]
-        public void addNewProduct()
-        {
-            using(UnitOfWork inst = UnitOfWork.get())
-            {
-                Product p = new Product()
-                {
-                    deptId = 2,
-                    groupId = 1,
-                    prodName = "לחמניה",
-                    salePrice = 1.5M,
-                };
-                inst.Products.Add(p);
-                inst.Complete();
-
-            }
-        }
+        
     }
 }
